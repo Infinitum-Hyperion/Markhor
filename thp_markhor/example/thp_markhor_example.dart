2714c4913e34dde437fab9a13eec0468561a9b6a
ObsidianXVI <siddharth.chitikela@gmail.com>
2023-07-04 18:50:06 +0800

    Initial commit

---
+++ thp_markhor/example/thp_markhor_example.dart
@@ -0,0 +1,6 @@
+import 'package:thp_markhor/thp_markhor.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ thp_markhor/test/thp_markhor_test.dart
@@ -0,0 +1,16 @@
+import 'package:thp_markhor/thp_markhor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
