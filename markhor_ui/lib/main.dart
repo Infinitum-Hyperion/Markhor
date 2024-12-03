3d3d53e1213a3a1c923d485fa5d6e332a3145004
ObsidianXVI <siddharth.chitikela@gmail.com>
2024-12-03 08:49:14 +0800

    create `markhor_ui`

---
+++ markhor_ui/lib/main.dart
@@ -0,0 +1,6 @@
+library autocloud.markhor;
+
+import 'package:flutter/material.dart';
+import 'package:autocloud_ui/design_system/design_system.dart';
+
+part './views/overview.dart';
---
+++ markhor_ui/lib/views/overview.dart
@@ -0,0 +1,10 @@
+part of autocloud.markhor;
+
+class MarkhorOverviewView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ACPGlobalScaffold(
+      child: SizedBox(),
+    );
+  }
+}
